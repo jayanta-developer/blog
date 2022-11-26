3c7690bfd58c32712adc68ea6d0e0575ef1c5413
jayanta dey <116143724+jayanta-musquare@users.noreply.github.com>
2022-11-26 14:08:12 +0530

    make a blog post with redux

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+//component
+import Post from "./Post";
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <Post />
+    </div>
+  );
+};
+
+export default App;
